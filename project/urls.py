563d5adb0154845c0144628730f7dfae782ab5ca
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-08-05 19:44:41 +0300

    Updated urls

--- project/urls.py
+++ project/urls.py
@@ -4,9 +4,6 @@ from django.urls import path
 from meetup_db import views
 
 urlpatterns = [
+    path('', admin.site.urls),
     path('admin/', admin.site.urls),
-    path('create/', views.create_user),
-    path('groups/<int:group_id>', views.get_groups),
-    path('events/', views.get_events),
-    path('user/<int:telegram_id>', views.user_detail),
 ]
