712d64ecf3bfd7003069e74954de01348d7a33d4
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-08-07 19:35:40 +0300

    FIX:  edited logic for the "get_user_status" function.

--- meetup_db/models.py
+++ meetup_db/models.py
@@ -245,11 +245,11 @@ def get_speaker(telegram_id: int) -> dict:
     return user_note
 
 
-def get_user_status(telegram_id: int) -> bool:
-    if guest := get_guest(telegram_id):
-        return guest['role']
-    elif speaker := get_speaker(telegram_id):
+def get_user_status(telegram_id: int):
+    if speaker := get_speaker(telegram_id):
         return speaker['role']
+    elif guest := get_guest(telegram_id):
+        return guest['role']
     else:
         return False
 
