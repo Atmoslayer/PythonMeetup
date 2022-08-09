474933568dc93d83f6e5ae6996b03a1dee3f7fd5
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-08-09 13:56:24 +0300

    Updated urls.py

--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,4 @@ from django.contrib import admin
 from django.urls import path
 
 
-urlpatterns = [
-    path('', admin.site.urls),
-    path('admin/', admin.site.urls),
-]
+urlpatterns = [path('', admin.site.urls)]
